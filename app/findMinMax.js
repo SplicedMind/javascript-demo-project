a5372fcd476b31af24a1809914b936acfdf0fa7a
Akinbode Femi <femi661@gmail.com>
2017-01-07 06:31:38 +0100

    Added comments to the code

--- app/findMinMax.js
+++ app/findMinMax.js
@@ -2,19 +2,26 @@ module.exports = {
  
  findMinMax: function(arr) {
    var minMax = [];
+   // Check if the argument is an array
    if (arr instanceof Array) 
       {
+         //Initialize min and max to the first array element
          var min = arr[0];
          var max = arr[0];
+         //Initialize iterator to 0
          var iterator = 0;
+         // Iteration of the array
          while(iterator < arr.length)
          {
+            //Check if the current element is a Number
             if(typeof(arr[iterator]) == 'number' )
             {
+               //Check if current element is less than min and assign current element to min if true
                if(min > arr[iterator])
                {
                   min = arr[iterator];
                }
+               //Check if the current element is greater than max and assign current element to max if true
                if (max < arr[iterator])
                {
                   max = arr[iterator];
@@ -22,16 +29,18 @@ module.exports = {
 
                iterator += 1;          
             }
+            //Returns `invalid argument if current element is not a number
             else
             {
                return 'invalid argument';
-            }
-            
+            }            
          }
+         //Checks if min equals max 
          if(min === max)
          {
             minMax[0] = min;
          }
+         //Execute if min does not equal max
          else
          {
             minMax[0] = min;
@@ -39,6 +48,7 @@ module.exports = {
          }
          return minMax;
       }
+      //Returns `invalid argument if argument is not an array
    else
       {
         return 'invalid argument';
